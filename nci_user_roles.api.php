859f440d0d0b99813608b4e95685f768170bcb63
Dan Young <youngdr@users.noreply.github.com>
2013-03-26 22:02:58 +0000

    Base user role module updates, still in progress.

--- nci_user_roles.api.php
+++ nci_user_roles.api.php
@@ -3,15 +3,10 @@
 /**
  * Use the provided array to add roles and update permissions to assign to each.
  * 
- * @param array $role_perm_map Array of the format role_name => permission name
- *                               => TRUE for each role and permission to
- *                               enable, all roles not enabled will be cleared.
- *                               (Provided for reference and to use existing
- *                                sets of permissions as a base for new roles.)   
  * @return array An array of roles and permissions to be added to the existing
  *               definitions.
  */
-function hook_user_roles_alter(array $role_perm_map) {
+function hook_role_permissions() {
     $new_perms = array();
     $new_perms['role_name']['permission name'] = true;
     $new_perms['second_role_name'] += array(
@@ -20,6 +15,25 @@ function hook_user_roles_alter(array $role_perm_map) {
     );
     
     return $new_perms;
+}
+
+/**
+ * Hook to retrieve all implementing modules' definitions of role permission
+ * inheritance.
+ * 
+ * @return array An array in the form $role => $inherited_perms, where each role
+ *               is mapped to an array of other roles whose permissions will
+ *               be aggregated and added to the base permissions for the role.
+ *               Each array should be the complete mapping for the module's
+ *               roles, (and in order of least to greatest access) as the 
+ *               permission assignment is done in a single pass without 
+ *               iteration.
+ */
+function hook_role_inheritance() {
+    return array(
+        'authenticated user' => array('anonymous user'),
+        'administrator' => array('anonymous user', 'authenticated user'),
+    );
 }
 
 ?>
